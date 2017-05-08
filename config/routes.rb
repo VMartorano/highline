5d20690c19426648c22a54347ed72a3252c207bb
Vincent Martorano <martoranov94@gmail.com>
2017-05-08 17:29:53 -0400

    added home routes, and reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
-  root to: "home#index"
+  root to: "home#index", as: "home"
+  get "/medical" => "home#medical"
+  get "/about" => "home#about"
+  get "/contact" => "home#contact"
+
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   #
   resources :products do
     resources :reviews
   end
+
+
 
 end
