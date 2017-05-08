c1d19e51893a3374ad99cb8ce096fb0c99df7e05
Marcela Pino <marcelap99@aol.com>
2017-05-08 17:35:26 -0400

    my commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  root to: "home#index"
+  root to: "home#index", as: "home"
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   #
   resources :products do
     resources :reviews
   end
+
+require 'bootstrap'
 
 end
